e26e334db567f3162d1a939fbfb5142168258fd2
Louis Skowronek <ulhni@student.kit.edu>
2023-07-29 00:01:43 +0200

    add file for onnx export

---
+++ models/onnx_export.py
@@ -0,0 +1,8 @@
+from ultralytics import YOLO
+
+# Load a model
+#model = YOLO('yolov8n.pt')  # load an official model
+model = YOLO('yolov8s_best.pt')  # load a custom trained
+
+# Export the model
+model.export(format='onnx', opset=15)
