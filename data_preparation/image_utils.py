1694494f2b7c430bcc08919222617948596732ea
Louis Skowronek <ulhni@student.kit.edu>
2023-08-01 14:52:08 +0200

    renamed file

--- data_preparation/image_utils.py
+++ data_preparation/image_utils.py
@@ -1,6 +1,8 @@
 import os
 from PIL import Image
 from tqdm import tqdm
+import pandas as pd
+import piexif
 
 
 def convert_image_to_png(image_dir, png_dir, drop_original=True):
@@ -47,5 +49,53 @@ def convert_image_to_jpeg(image_dir, jpeg_dir, drop_original=True):
             os.remove(image_path)
 
         image.convert("RGB").save(jpeg_path, "JPEG")
+
+
+
+def remove_exif(dir_images):
+
+    image_extensions = [".jpg", ".jpeg", ".png", ".gif"]
+    image_files = [file for file in os.listdir(dir_images) if os.path.isfile(os.path.join(dir_images, file)) and any(file.lower().endswith(ext) for ext in image_extensions)]
+
+    df_img = pd.DataFrame(columns=['image_path', 'rotation'])
+
+    for i, img in enumerate(image_files):
+        filepath = os.path.join(dir_images, img)
+        image = Image.open(filepath)
+
+        # Get the EXIF data
+        exif_data = image.info.get("exif")
+
+        # Check if EXIF data exists
+        if exif_data:
+            # Convert the EXIF data to a mutable dictionary
+            exif_dict = piexif.load(exif_data)
+            # print(exif_dict.keys())
+            # Check if the orientation tag exists
+            if 274 in exif_dict["0th"]:
+                # Update the orientation tag to 0 (normal)
+                print(filepath)
+                print(f'{i}: orientation is {exif_dict["0th"][274]} --- Set to 0')
+
+                # Store information
+                df_img.loc[i, ['image_path', 'rotation']] = [filepath, exif_dict["0th"][274]]
+
+                # Set exif direction to 0
+                exif_dict["0th"][274] = 0
+
+                # Convert the EXIF data back to bytes
+                exif_bytes = piexif.dump(exif_dict)
+
+                # Save the image with updated EXIF data
+                image.save(filepath) #, exif=exif_bytes)
+
+
+            else:
+                print("No orientation tag found.")
+
+        else:
+            print("No EXIF data found.")
+
+    return df_img
 
 
