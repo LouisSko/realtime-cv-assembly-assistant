4fe930df2025674ab64ae208fefd13d30aeae791
Louis Skowronek <ulhni@student.kit.edu>
2023-08-13 17:50:13 +0200

    code cleanup

--- train/export_model.py
+++ train/export_model.py
@@ -1,8 +1,25 @@
-from ultralytics import YOLO
+def main():
+    """
+    Main function to load a YOLO model and export it to ONNX format.
+    """
+    try:
+        # Import the YOLO class from the ultralytics module
+        from ultralytics import YOLO
 
-# Load a model
-#model = YOLO('yolov8n.pt')  # load an official model
-model = YOLO('../models/yolov8s_best.pt')  # load a custom trained
+        # Load a YOLO model (replace the model path with your own)
+        #model = YOLO('yolov8n.pt')  # load an official model
+        model = YOLO('../models/yolov8s_best.pt')  # load a custom trained model
 
-# Export the model
-model.export(format='onnx', opset=15)
+        # Export the YOLO model to ONNX format with specific opset version
+        model.export(format='onnx', opset=15)
+
+        print("Model exported to ONNX format successfully.")
+
+    except ImportError:
+        print("Please make sure you have the ultralytics library installed.")
+    except Exception as e:
+        print("An error occurred:", e)
+
+
+if __name__ == "__main__":
+    main()
--- train/train_model.py
+++ train/train_model.py
@@ -1,15 +1,34 @@
-from ultralytics import YOLO
-
-
-# load a pretrained model
-
 def train_yolo_v8():
-    model = YOLO("yolov8n.pt")
+    """
+    Train a YOLO v8 model using Ultralytics.
 
-    model.train(data="/Users/louis.skowronek/object-detection-project/aiss_yolo.yaml",
-                pretrained=True,
-                epochs=5,
-                name='yolov8n_custom')
+    This function loads a pretrained YOLO v8 model and trains it further using the specified parameters.
+    """
+    try:
+        # Import the YOLO class from the ultralytics module
+        from ultralytics import YOLO
+
+        # Load a pretrained YOLO v8 model
+        model = YOLO("yolov8n.pt")
+
+        # Train the model with specified parameters
+        model.train(
+            data="/Users/louis.skowronek/object-detection-project/aiss_yolo.yaml",
+            pretrained=True,
+            epochs=100,
+            device='0',  # use GPU
+            name='yolov8n_custom',
+            flipud=0.5,
+            degrees=0.8
+        )
+
+        print("Training completed successfully.")
+
+    except ImportError:
+        print("Please make sure you have the ultralytics library installed.")
+    except Exception as e:
+        print("An error occurred:", e)
+
 
 if __name__ == '__main__':
     train_yolo_v8()
