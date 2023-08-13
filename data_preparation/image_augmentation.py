e806046dd766d77e055e584ca8def90747c72de4
Louis Skowronek <ulhni@student.kit.edu>
2023-08-13 16:20:09 +0200

    add documentation

--- data_preparation/image_augmentation.py
+++ data_preparation/image_augmentation.py
@@ -7,99 +7,107 @@ import argparse
 
 
 def image_augmentation(input_dir, output_dir, nr_of_augs=5):
+    """
+    Apply image augmentation to a dataset of images and their corresponding YOLO annotations.
 
+    This function reads images and their YOLO annotations, applies a series of augmentations,
+    and saves the augmented images and updated annotations to the specified output directory.
+
+    :param input_dir (str): Directory containing the 'images' and 'labels' subdirectories with original data.
+    :param output_dir (str): Directory where augmented 'images' and 'labels' will be saved.
+    :param nr_of_augs (int): Number of augmentations to create for each image.
+    :return: None
+    """
+
+    # Define paths for original images and annotations
     dir_images = os.path.join(input_dir, 'images')
     dir_annot = os.path.join(input_dir, 'labels')
 
+    # Define paths for augmented images and annotations
     dir_aug_images = os.path.join(output_dir, 'images')
     dir_aug_annot = os.path.join(output_dir, 'labels')
 
-    # Check if the directories exist, create them if necessary
+    # Check if the output directories exist, create them if necessary
     for dir_path in [output_dir, dir_aug_images, dir_aug_annot]:
         if not os.path.exists(dir_path):
             os.makedirs(dir_path)
 
-    # Define augmentation sequence
+    # Define augmentation sequence for images
     seq = augmenters.Sequential([
-        augmenters.Fliplr(p=0.5),  # Horizontal flipping
-        augmenters.Flipud(p=0.5),  # Vertical flipping
+        augmenters.Fliplr(p=0.5),
+        augmenters.Flipud(p=0.5),
         augmenters.Affine(rotate=(-45, 45)),
-        # augmenters.AdditiveGaussianNoise(scale=(0, 0.1 * 255)),  # Add Gaussian 
-        augmenters.GammaContrast(gamma=(0.5, 2.0)),  # Gamma correction for contrast adjustment
+        augmenters.GammaContrast(gamma=(0.5, 2.0)),
         augmenters.Multiply((0.8, 1.2))
-        # augmenters.Crop(percent=(0, 0.2)),  # Random cropping
     ])
 
-    # List of image file extensions to consider
+    # List of valid image file extensions
     image_extensions = [".jpg", ".jpeg", ".png", ".gif"]
 
-    # Get all image files in the directory
+    # Get list of image files in the directory
     image_files = [file for file in os.listdir(dir_images) if os.path.isfile(os.path.join(dir_images, file)) and any(
         file.lower().endswith(ext) for ext in image_extensions)]
 
-    # Iterate over all files
+    # Iterate over all image files
     for img in tqdm(image_files):
 
-        # get txt file
+        # Determine corresponding annotation file for the image
         txt = img.split('.')[0] + '.txt'
 
-        # check if a .txt file exists
+        # Check if annotation file exists for the image
         if os.path.isfile(os.path.join(dir_annot, txt)):
 
-            # Load original image and bounding box annotations
+            # Load the original image and its bounding box annotations
             image = cv2.imread(os.path.join(dir_images, img))
-
             boxes = read_yolo_file(os.path.join(dir_annot, txt))
 
-            # Convert yolo format to bounding box format
-            boxes_conv = convert_yolo_to_bbf(boxes, pixel_height=image.shape[0], pixel_width=image.shape[1], formatBoundingBox=True)
-
-            # Convert bounding box coordinates to imgaug format
+            # Convert YOLO format bounding boxes to imgaug's format
+            boxes_conv = convert_yolo_to_bbf(boxes, pixel_height=image.shape[0], pixel_width=image.shape[1],
+                                             formatBoundingBox=True)
             bbs = BoundingBoxesOnImage(boxes_conv, shape=image.shape)
 
-            # Create augmentations for one image nr_of_augs times
+            # Apply augmentations for each image 'nr_of_augs' times
             for it in range(nr_of_augs):
 
-                # Apply augmentation
+                # Apply the defined augmentation sequence
                 aug_image, aug_bbs = seq(image=image, bounding_boxes=bbs)
-                # aug_image = cv2.cvtColor(aug_image, cv2.COLOR_BGR2RGB)
 
-                # Convert back to yolo - only store bbs which are fully visible in the image
+                # Convert the augmented bounding boxes back to YOLO format
                 boxes_new = convert_bbf_to_yolo(aug_bbs)
 
-                # Only store image, if it contains any boxes
+                # Save augmented image and annotations only if there are valid bounding boxes
                 if len(boxes_new) > 0:
-
-                    # Generate unique file names for each augmentation
                     aug_image_name = f"{img.split('.')[0]}_{it}.jpeg"
                     aug_annot_name = f"{img.split('.')[0]}_{it}.txt"
-
-                    # save augmented image
                     cv2.imwrite(os.path.join(dir_aug_images, aug_image_name), aug_image)
                     save_yolo_file(os.path.join(dir_aug_annot, aug_annot_name), boxes_new)
 
-            # save original image
-            # original_image.save(os.path.join(dir_aug_images, img))
-            # save_yolo_file(os.path.join(dir_aug_annot, txt), boxes)
-
 
 if __name__ == "__main__":
-    # Set up argument parser
+    # Check if this script is the entry point to the program
+    # This ensures the code below is only executed if the script is run directly, not when imported
+
+    # Initialize an argument parser
     parser = argparse.ArgumentParser(description="Augment Images")
 
+    # Add argument for specifying the input directory which should contain 'images' and 'labels' subdirectories
     parser.add_argument("--input_dir", default="/Users/louis.skowronek/AISS/aiss_images/train",
                         help="Input directory path. Should contain a folder images and labels")
+
+    # Add argument for specifying the output directory
     parser.add_argument("--output_dir", default=None,
                         help="Output directory path. If None, it will be the same as input_dir_base")
+
+    # Add argument for specifying the number of augmentations per image
     parser.add_argument("--nr_of_augs", type=int, default=10,
                         help="Number of augmented images per image")
 
+    # Parse the command-line arguments
     args = parser.parse_args()
 
-    # If no output directory is provided, use the input directory as the output directory
+    # Check if an output directory is specified. If not, set it to be the same as the input directory
     if args.output_dir is None:
         args.output_dir = args.input_dir
 
-
-    # Call function with command-line arguments
+    # Call the image augmentation function with the parsed command-line arguments
     image_augmentation(args.input_dir, args.output_dir, args.nr_of_augs)
