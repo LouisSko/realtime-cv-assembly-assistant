23aa22823b637fb146e988c3b9d56c918a575b10
Louis Skowronek <ulhni@student.kit.edu>
2023-08-13 17:04:43 +0200

    code cleanup for draw_detections function

--- onnx_yolov8/utils.py
+++ onnx_yolov8/utils.py
@@ -2,59 +2,79 @@ import numpy as np
 import cv2
 import os
 
-
-
 # Get the directory path of the current file
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Construct the relative path to the file you want to access
 labels_path = os.path.join(current_dir, 'classes.txt')
 
+# Attempt to read class names from the file
 try:
     class_names = [name.strip() for name in open(labels_path).readlines()]
 except:
     print(f"Error while reading {labels_path}. Go into utils.py to change the path to classes.txt")
 
-
-# Create a list of colors for each class where each color is a tuple of 3 integer values
+# Create a list of unique colors for visualization, one for each class
+# Each color is a tuple of 3 integer values
 rng = np.random.default_rng(3)
 colors = rng.uniform(0, 255, size=(len(class_names), 3))
 
 def nms(boxes, scores, iou_threshold):
-    # Sort by score
+    """
+    Perform Non-Maximum Suppression on bounding boxes.
+
+    Args:
+        boxes (np.array): Bounding boxes.
+        scores (np.array): Confidence scores for each box.
+        iou_threshold (float): Overlap threshold for suppression.
+
+    Returns:
+        list: Indices of bounding boxes to keep.
+    """
+    # Sort boxes by score in descending order
     sorted_indices = np.argsort(scores)[::-1]
 
     keep_boxes = []
     while sorted_indices.size > 0:
-        # Pick the last box
+        # Select the box with the highest score
         box_id = sorted_indices[0]
         keep_boxes.append(box_id)
 
-        # Compute IoU of the picked box with the rest
+        # Compute IoU of the selected box with the rest
         ious = compute_iou(boxes[box_id, :], boxes[sorted_indices[1:], :])
 
-        # Remove boxes with IoU over the threshold
+        # Retain boxes with IoU less than the threshold
         keep_indices = np.where(ious < iou_threshold)[0]
-
-        # print(keep_indices.shape, sorted_indices.shape)
         sorted_indices = sorted_indices[keep_indices + 1]
 
     return keep_boxes
 
-
 def compute_iou(box, boxes):
-    # Compute xmin, ymin, xmax, ymax for both boxes
+    """
+    Compute Intersection over Union (IoU) between a box and a list of boxes.
+
+    Args:
+        box (np.array): A single bounding box.
+        boxes (np.array): A list of bounding boxes.
+
+    Returns:
+        np.array: IoU values for the box with each box in the list.
+    """
+
+    # Compute intersection coordinates
     xmin = np.maximum(box[0], boxes[:, 0])
     ymin = np.maximum(box[1], boxes[:, 1])
     xmax = np.minimum(box[2], boxes[:, 2])
     ymax = np.minimum(box[3], boxes[:, 3])
 
-    # Compute intersection area
+    # Compute area of intersection
     intersection_area = np.maximum(0, xmax - xmin) * np.maximum(0, ymax - ymin)
 
-    # Compute union area
+    # Compute areas of the boxes
     box_area = (box[2] - box[0]) * (box[3] - box[1])
     boxes_area = (boxes[:, 2] - boxes[:, 0]) * (boxes[:, 3] - boxes[:, 1])
+
+    # Compute union area
     union_area = box_area + boxes_area - intersection_area
 
     # Compute IoU
@@ -64,81 +84,114 @@ def compute_iou(box, boxes):
 
 
 def xywh2xyxy(x):
-    # Convert bounding box (x, y, w, h) to bounding box (x1, y1, x2, y2)
+    """
+    Convert bounding box format from (x_center, y_center, width, height) to
+    (x1, y1, x2, y2) where (x1,y1) is the top-left corner and (x2,y2) is the
+    bottom-right corner.
+
+    Args:
+        x (np.array): Input array containing bounding boxes in (x_center, y_center, width, height) format.
+
+    Returns:
+        np.array: Array containing bounding boxes in (x1, y1, x2, y2) format.
+    """
+
+    # Create a copy of the input to prevent modifying the original data
     y = np.copy(x)
-    y[..., 0] = x[..., 0] - x[..., 2] / 2
+
+    # Convert center x, y to top-left x, y
+    y[..., 0] = x[..., 0] - x[..., 2] / 2  # --> a[0, :, :] or simply a[0, ...]
     y[..., 1] = x[..., 1] - x[..., 3] / 2
+
+    # Convert width and height to bottom-right x, y
     y[..., 2] = x[..., 0] + x[..., 2] / 2
     y[..., 3] = x[..., 1] + x[..., 3] / 2
+
     return y
 
 
-def draw_detections(image, boxes, scores, class_ids, required_class_ids=None, mask_alpha=0.3, multi_color=False, displayAll=False, displayConfidence=False, displayLabel=False):
+def draw_detections(image, boxes, scores, class_ids, required_class_ids=None, mask_alpha=0.3, multi_color=False,
+                    displayAll=False, displayConfidence=False, displayLabel=False):
+    """
+    Draw bounding boxes and labels on an image.
 
+    Args:
+        image (numpy.ndarray): The input image.
+        boxes (list): List of bounding boxes.
+        scores (list): List of scores corresponding to each bounding box.
+        class_ids (list): List of class IDs corresponding to each bounding box.
+        required_class_ids (list, optional): List of class IDs that are required to be displayed.
+        mask_alpha (float, optional): Alpha value for overlaying the detection mask on the original image.
+        multi_color (bool, optional): If True, use a different color for each class.
+        displayAll (bool, optional): If True, display all detections irrespective of `required_class_ids`.
+        displayConfidence (bool, optional): If True, display the confidence score of each detection.
+        displayLabel (bool, optional): If True, display the class label of each detection.
+
+    Returns:
+        numpy.ndarray: The image with detections drawn.
+    """
+
+    # Make a copy of the input image for drawing detections and mask
     mask_img = image.copy()
     det_img = image.copy()
 
+    # Determine image dimensions
     img_height, img_width = image.shape[:2]
+
+    # Define scale and thickness based on image dimensions
     size = min([img_height, img_width]) * 0.0005
     text_thickness = int(min([img_height, img_width]) * 0.001)
 
-    # Draw bounding boxes and labels of detections
+    # Iterate over bounding boxes, scores, and class IDs
     for box, score, class_id in zip(boxes, scores, class_ids):
         label = class_names[class_id]
 
-        # Check if every part is displayed or only required ones
-        if (required_class_ids is not None and label in required_class_ids) or (required_class_ids is None) or (displayAll is True):
-            x1, y1, x2, y2 = box.astype(int)
+        # Determine whether to display the detection
+        should_display = (
+                (required_class_ids is not None and label in required_class_ids) or
+                (required_class_ids is None) or
+                (displayAll is True)
+        )
+        if not should_display:
+            continue
 
-            # Settings
-            if multi_color == False:
-                if displayAll is True and required_class_ids is not None and label not in required_class_ids:
-                     # Set color
-                    color = (179, 179, 179)
-                else:
-                    # Set color
-                    color = (102, 102, 255)
-                
-                # Draw rectangle
-                cv2.rectangle(det_img, (x1, y1), (x2, y2), color, 2)
-                # Draw fill rectangle in mask image
-                cv2.rectangle(mask_img, (x1, y1), (x2, y2), color, -1)
-            else:
-                # Set color
-                color = colors[class_id]
-                # Draw rectangle
-                cv2.rectangle(det_img, (x1, y1), (x2, y2), color, 2)
-                # Draw fill rectangle in mask image
-                cv2.rectangle(mask_img, (x1, y1), (x2, y2), color, -1)
-            
-            # Labels based on settings
-            if displayConfidence == True and displayLabel == True:
-                caption = f'{label} {int(score * 100)}%'
-            elif  displayLabel == True and displayConfidence == False:
-                caption = f'{label}'
-            elif displayConfidence == True and displayLabel == False:
-                caption = f'Required piece: {int(score * 100)}%'
-            else:
-                caption = "Required piece"
+        # Convert box coordinates to integer
+        x1, y1, x2, y2 = box.astype(int)
 
-            (tw, th), _ = cv2.getTextSize(text=caption, fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-                                        fontScale=size, thickness=text_thickness)
-            th = int(th * 1.2)
+        # Determine the color for the bounding box
+        color = colors[class_id] if multi_color else (102, 102, 255)
+        if displayAll and required_class_ids and label not in required_class_ids:
+            color = (179, 179, 179)
 
-            cv2.rectangle(det_img, (x1, y1),
-                        (x1 + tw, y1 - th), color, -1)
-            cv2.rectangle(mask_img, (x1, y1),
-                        (x1 + tw, y1 - th), color, -1)
-            cv2.putText(det_img, caption, (x1, y1),
-                        cv2.FONT_HERSHEY_SIMPLEX, size, (255, 255, 255), text_thickness, cv2.LINE_AA)
+        # Draw bounding box and filled rectangle in mask image
+        cv2.rectangle(det_img, (x1, y1), (x2, y2), color, 2)
+        cv2.rectangle(mask_img, (x1, y1), (x2, y2), color, -1)
 
-            cv2.putText(mask_img, caption, (x1, y1),
-                        cv2.FONT_HERSHEY_SIMPLEX, size, (255, 255, 255), text_thickness, cv2.LINE_AA)
+        # Determine the caption based on display settings
+        if displayConfidence and displayLabel:
+            caption = f'{label} {int(score * 100)}%'
+        elif displayLabel:
+            caption = f'{label}'
+        elif displayConfidence:
+            caption = f'Required piece: {int(score * 100)}%'
+        else:
+            caption = "Required piece"
 
+        # Draw the caption on the image
+        (tw, th), _ = cv2.getTextSize(text=caption, fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                                      fontScale=size, thickness=text_thickness)
+        th = int(th * 1.2)
+        cv2.rectangle(det_img, (x1, y1), (x1 + tw, y1 - th), color, -1)
+        cv2.putText(det_img, caption, (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, size, (255, 255, 255), text_thickness,
+                    cv2.LINE_AA)
+
+        # Draw the caption on the mask image
+        cv2.rectangle(mask_img, (x1, y1), (x1 + tw, y1 - th), color, -1)
+        cv2.putText(mask_img, caption, (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, size, (255, 255, 255), text_thickness,
+                    cv2.LINE_AA)
+
+    # Merge the mask and the detection images
     return cv2.addWeighted(mask_img, mask_alpha, det_img, 1 - mask_alpha, 0)
-
-
-
 
 
 class MotionDetector:
